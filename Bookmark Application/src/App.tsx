cada987a100f8e436e2538ac3cbcb2e91dd28083
harshgupta372 <167362291+harshgupta372@users.noreply.github.com>
2024-10-26 17:51:11 +0530

    Add files via upload

---
+++ Bookmark Application/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BookmarkManager } from './components/BookmarkManager';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <BookmarkManager />
+    </div>
+  );
+}
+
+export default App;
